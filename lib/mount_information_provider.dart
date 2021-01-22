8ccdd0d1c7256506cf9bd136bded5c24e57704ee
HeavenOSK <heavenosk@gmail.com>
2021-01-22 20:57:18 +0900

    create project

---
+++ lib/mount_information_provider.dart
@@ -0,0 +1,3 @@
+library mount_information_provider;
+
+export 'src/mount_information_provider.dart';
---
+++ lib/src/mount_information_provider.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/rendering.dart';
+import 'package:flutter/widgets.dart';
+
+/// A widget which provides mount information of child.
+///
+///
+class MountInformationProvider extends SingleChildRenderObjectWidget {
+  const MountInformationProvider({
+    Key key,
+
+    /// The widget which is wanted to know the mount information.
+    Widget child,
+    @required this.onRegionChanged,
+  })  : assert(onRegionChanged != null),
+        super(key: key, child: child);
+
+  /// A callback to notify mount information of child.
+  final void Function(
+    Offset position,
+    Size size,
+  ) onRegionChanged;
+
+  @override
+  _MountInformationListnableRenderObject createRenderObject(
+      BuildContext context) {
+    return _MountInformationListnableRenderObject(
+      onRegionChanged: onRegionChanged,
+    );
+  }
+}
+
+class _MountInformationListnableRenderObject extends RenderProxyBox {
+  _MountInformationListnableRenderObject({
+    @required this.onRegionChanged,
+  })  : assert(onRegionChanged != null),
+        super(null);
+
+  final void Function(
+    Offset position,
+    Size size,
+  ) onRegionChanged;
+
+  Size _currentSize;
+  Offset _currentPosition;
+
+  @override
+  void paint(PaintingContext context, Offset offset) {
+    super.paint(context, offset);
+    _notifyMountInformation();
+  }
+
+  @override
+  void performLayout() {
+    super.performLayout();
+    _notifyMountInformation();
+  }
+
+  void _notifyMountInformation() {
+    Future(() {
+      final pos = this.localToGlobal(Offset.zero);
+      final size = this.size;
+
+      if (size != _currentSize || pos != _currentPosition) {
+        _currentSize = size;
+        _currentPosition = pos;
+        onRegionChanged?.call(pos, size);
+      }
+    });
+  }
+}
